94018058962a1ee903e390c862c62bbca25f6120
nitinravindran <nitinravindran@yahoo.co.in>
2020-10-09 19:40:29 +0530

    Minor update

--- SeleniumTestAutomationRunner(STAR)/src/main/java/com/star/core/BrowserConfig.java
+++ SeleniumTestAutomationRunner(STAR)/src/main/java/com/star/core/BrowserConfig.java
@@ -38,7 +38,6 @@ public class BrowserConfig {
             options.addArguments("disable-browser-side-navigation");
             options.addArguments("disable-gpu");
         } else if (browser.equalsIgnoreCase(String.valueOf(Browser.CHROME_HEADLESS))) {
-            //options.setHeadless(true);
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-setuid-sandbox");
             options.addArguments("--remote-debugging-port=9222");
@@ -48,6 +47,7 @@ public class BrowserConfig {
             options.addArguments("start-maximized");
             options.addArguments("disable-infobars");
             options.addArguments("--headless");
+            //options.setHeadless(true);
             //options.addArguments("window-size=1980,960");
             //options.setBinary("/usr/bin/chromium-browser");
         }
